4ffee7a310fea4861e48f9a80f128452fbcac662
kupina <kupina@yandex.ru>
2022-08-17 16:45:04 +0300

    Поправила решение задачи 27 для отрицательных чисел

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -8,11 +8,12 @@
 Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
+int absol = Math.Abs(number);
 
-while (number > 0)
+while (absol > 0)
 {
-int num = number % 10;
-number = number / 10;
+int num = absol % 10;
+absol = absol / 10;
 sum = sum + num;
 }
 Console.WriteLine("Сумма всех цифр в данном числе равна: " + sum);
