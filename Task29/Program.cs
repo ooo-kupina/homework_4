55bde67c52ac47852affbc65ab232ffed20e1b5b
kupina <kupina@yandex.ru>
2022-08-17 07:27:49 +0300

    добавила задачу Task29 и .gitignor

---
+++ Task29/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < 9)
+    {
+        collection[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write("[");
+    while (position < count-1)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+    Console.Write($"{col[position]}");
+    Console.Write("]");
+}
+
+int[] array = new int[9];
+
+FillArray(array);
+PrintArray(array);
+
